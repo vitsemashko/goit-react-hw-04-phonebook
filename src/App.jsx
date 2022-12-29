741ce37f19bf86add503b2e6b4ef77b1924465fe
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2022-12-29 19:38:07 +0200

    Update App.jsx
    Added initialContacts as external json file
    Added logic which denied adding the same name of contact to contacts list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 import './App.css';
+import initialContacts from './components/initialContacts';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
 
 const App = () => {
-  const [contacts, setContacts] = React.useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = React.useState(initialContacts);
   const [name, setName] = React.useState('');
   const [number, setNumber] = React.useState('');
   const [filter, setFilter] = React.useState('');
@@ -40,16 +36,24 @@ const App = () => {
   React.useEffect(() => {
     onContactsFilter(); // eslint-disable-next-line
   }, [filter]);
-
   const onHandleSubmit = e => {
     e.preventDefault();
-    setVisibleContacts(prevState => [
-      ...prevState,
-      { id: nanoid(), name, number },
-    ]);
-    setContacts(prevState => [...prevState, { id: nanoid(), name, number }]);
-    setName('');
-    setNumber('');
+    let checked = contacts.find(contact => {
+      return contact.name.toLowerCase() === name.toLowerCase();
+    });
+    if (checked) {
+      alert('Current username is alredy in the list');
+      setName('');
+      setNumber('');
+    } else {
+      setVisibleContacts(prevState => [
+        ...prevState,
+        { id: nanoid(), name, number },
+      ]);
+      setContacts(prevState => [...prevState, { id: nanoid(), name, number }]);
+      setName('');
+      setNumber('');
+    }
   };
   return (
     <div>
